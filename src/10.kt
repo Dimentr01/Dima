e58f350645f8bd3309854ea4256dc4b3e255b4ae
Dimentr01 <dima.zaharcev@mail.ru>
2018-06-01 14:57:45 +0300

    vg

---
+++ src/10.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) data class Player @JvmOverloads constructor(var name: String, var lvl: Int=1, var hp: Int=10.0, exp: Int= 10.0({
+    private fun DeathPenalty() {
+        var hp = hp / 20
+        var exp = exp / 2
+    }
+
+    fun DeathPenaltly(newhp: Int, newexp: Int) {
+        print("Oh, my Goodness!!!I am death!!!")
+    }
+
+}
+
+        
---
+++ src/PlayerTest.groovy
@@ -0,0 +1,28 @@
+class PlayerTest extends groovy.util.GroovyTestCase {
+    void testGetName() {
+    }
+
+    void testSetName() {
+    }
+
+    void testGetLvl() {
+    }
+
+    void testSetLvl() {
+    }
+
+    void testGetHp() {
+    }
+
+    void testSetHp() {
+    }
+
+    void testGetExp() {
+    }
+
+    void testSetExp() {
+    }
+
+    void testComponent1() {
+    }
+}
